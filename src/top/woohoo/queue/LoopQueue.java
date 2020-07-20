d59dbd61c6665bbfb1a44feed3938c8195653e25
Emmett Woo <Emmettwoo@Outlook.com>
2020-07-20 15:08:07 +0800

    LoopQueue Optimization.

--- src/top/woohoo/queue/LoopQueue.java
+++ src/top/woohoo/queue/LoopQueue.java
@@ -8,11 +8,11 @@ public class LoopQueue<T> implements Queue<T> {
 
     // Constructors
     public LoopQueue() {
-        this.data = (T[])new Object[capacity + 1];
+        this.data = (T[])new Object[capacity];
     }
     public LoopQueue(int capacity) {
         this.capacity = capacity;
-        this.data = (T[])new Object[capacity + 1];
+        this.data = (T[])new Object[capacity];
     }
 
 
@@ -35,16 +35,17 @@ public class LoopQueue<T> implements Queue<T> {
             this.expansion();
         }
         this.data[tail % capacity] = value;
-        tail++;
+        tail = (tail + 1) % capacity;
     }
 
     @Override
     public T dequeue() {
         if (!this.isEmpty()) {
             T value = this.data[front];
-            this.data[front++] = null;
+            this.data[front] = null;
+            front = (front + 1) % capacity;
 
-            if (this.getSize() <= capacity/4 ) {
+            if (this.getSize() <= capacity/4) {
                 this.shrinkage();
             }
 
@@ -74,14 +75,17 @@ public class LoopQueue<T> implements Queue<T> {
         }
     }
     private void shrinkage() {
-        this.capacity /= 2;
-        // 复制旧data
-        int queueSize = this.getSize();
-        T[] newData = (T[])new Object[capacity];
-        for (int index = 0; index < queueSize; index++) {
-            newData[(front + index) % capacity] = this.data[(front + index) % (capacity * 2)];
+        if (this.capacity/2 > 0) {
+            // 复制旧data
+            int queueSize = this.getSize();
+            this.capacity /= 2;
+            T[] newData = (T[])new Object[capacity];
+            for (int index = 0; index < queueSize; index++) {
+                newData[(front + index) % capacity] = this.data[(front + index) % (capacity * 2)];
+            }
+            this.data = newData;
+            front%=capacity;
         }
-        this.data = newData;
     }
 
 
@@ -98,7 +102,7 @@ public class LoopQueue<T> implements Queue<T> {
                     dataString.append(", ");
                 }
             }
-            return "Queue capacity: " + this.capacity + ", data: [ " + dataString + " ] -> end";
+            return "Queue data: [ " + dataString + " ] -> end";
         }
     }
 }
